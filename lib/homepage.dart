244afec1264553e22f852479184cd15ceb2bd539
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-04-26 14:08:39 +0530

    fixing top container

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:new_weather_app/widgets/bg-color-widgets.dart';
 
@@ -7,6 +9,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> weekday = ['Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday'];
+  List<String> months = [
+    'December',
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November'    
+  ];
+
+  int minute;
+  String minuteFormat() {
+    if(DateTime.now().minute - 10 < 0) {
+      return '0${DateTime.now().minute}';
+    } else {
+      return '${DateTime.now().minute}';
+    }
+  }
+
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        
+      });
+    });
+    super.initState();
+  }
+
+
+
+  //------------- UI OF THE APP -------------//
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +60,41 @@ class _HomePageState extends State<HomePage> {
               children: [
                 //---------- TOP CONTAINER ----------//
                 Container(
+                  padding: EdgeInsets.only(left: 20),
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.width / 3.5,                  
-                  child: Text('Top Container'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${DateTime.now().hour}:${minuteFormat()}, ${weekday[DateTime.now().weekday].substring(0, 3)} ${months[DateTime.now().month].substring(0, 3)} ${DateTime.now().day}',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            'Kolkata, IN',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ],
+                      ),
+                      MaterialButton(
+                        onPressed: () => print('Search'),
+                        child: Icon(Icons.search, color: Colors.white,),
+                        color: Colors.black12,
+                        elevation: 0,
+                        shape: CircleBorder(),
+                      )
+                    ],
+                  ),
                 ),
 
                 //---------- BOTTOM CONTAINER ----------//
