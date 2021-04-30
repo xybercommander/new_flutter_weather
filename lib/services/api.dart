8d29c8a24c75d40903401a8eea0ddbb70c238608
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-04-30 17:36:30 +0530

    fixed the api key privacy

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:new_weather_app/modules/api_constants.dart';
 import 'package:new_weather_app/modules/weather_constants.dart';
 
 class WeatherApi {
@@ -10,13 +11,13 @@ class WeatherApi {
     try {
 
       // TODO: Remove the API KEY from the github repo
-      var max_min_url = Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=hourly,minutely,alerts,current&appid=fc068a7084e8ead061063e8028105b99');
+      var max_min_url = Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=hourly,minutely,alerts,current&appid=${ApiConstants.API_KEY}');
       Response response = await get(max_min_url);
       var data = jsonDecode(response.body);
       WeatherConstants.min_temp = (data['daily'][0]['temp']['min'] - 273.15).round();
       WeatherConstants.max_temp = (data['daily'][0]['temp']['max'] - 273.15).round();
       
-      var hourly_weather = Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=current,daily,minutely,alerts&appid=fc068a7084e8ead061063e8028105b99');
+      var hourly_weather = Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=current,daily,minutely,alerts&appid=${ApiConstants.API_KEY}');
 
 
     } catch(e) {
